34b695fb4fcf40b4e69704a46c6e7b9b204505af
Clay Branch <clay.branch@gmail.com>
2017-01-07 14:11:15 -0600

    removed boilerplate code

--- electron-app/app/reducers/index.js
+++ electron-app/app/reducers/index.js
@@ -3,11 +3,9 @@ import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import { reducer as form } from 'redux-form';
 import client from '../apollo-client';
-import counter from './counter';
 
 const rootReducer = combineReducers({
   apollo: client.reducer(),
-  counter,
   routing,
   form,
 });
